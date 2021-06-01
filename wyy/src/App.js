281c21071ba3d4ce74be8fb833d33a30c44741a7
xipper <864353360@qq.com>
2021-06-01 20:26:40 +0800

    add svg

---
+++ wyy/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { GlobalStyle } from  './cssInJs';
+import { IconStyle } from './assets/iconfont/iconfont';
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <IconStyle />
+    </div>
+  );
+}
+
+export default App;
